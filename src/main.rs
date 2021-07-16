6f0028d990c5384592420606fc3896e084c2485f
aQaTL <mmsoltys@outlook.com>
2021-07-16 21:29:56 +0200

    Scan image and save raw data to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::bail;
 use log::{debug, info};
-use std::ffi::CStr;
+use std::ffi::{c_void, CStr};
 
 mod sane {
 	#![allow(non_upper_case_globals)]
@@ -76,5 +76,239 @@ fn main() -> anyhow::Result<()> {
 		}
 	}
 
+	if device_list.is_empty() {
+		return Ok(());
+	}
+
+	let mut device_handle: sane::SANE_Handle = std::ptr::null_mut();
+	let status: sane::SANE_Status = unsafe {
+		libsane.sane_open(
+			(*device_list[0]).name,
+			&mut device_handle as *mut sane::SANE_Handle,
+		)
+	};
+	if status != sane::SANE_Status_SANE_STATUS_GOOD {
+		bail!("Failed to open device {}.", status);
+	}
+	let device_handle = unsafe { Device::from_raw_handle(device_handle, &libsane) };
+
+	let option_count_descriptor = unsafe { libsane.sane_get_option_descriptor(device_handle.0, 0) };
+	if option_count_descriptor.is_null() {
+		bail!("Failed to get option count");
+	}
+
+	unsafe {
+		info!(
+			"{:?}",
+			CStr::from_ptr((*option_count_descriptor).name).to_string_lossy()
+		);
+		info!(
+			"{:?}",
+			CStr::from_ptr((*option_count_descriptor).title).to_string_lossy()
+		);
+		info!(
+			"{:?}",
+			CStr::from_ptr((*option_count_descriptor).desc).to_string_lossy()
+		);
+		info!("{:?}", (*option_count_descriptor).type_);
+		info!("{:?}", (*option_count_descriptor).unit);
+		info!("{:?}", (*option_count_descriptor).size);
+		info!("{:?}", (*option_count_descriptor).cap);
+		info!("{:?}", (*option_count_descriptor).constraint_type);
+	}
+
+	let mut option_count_value = 0_i32;
+	let status: sane::SANE_Status = unsafe {
+		libsane.sane_control_option(
+			device_handle.0,
+			0,
+			sane::SANE_Action_SANE_ACTION_GET_VALUE,
+			(&mut option_count_value as *mut i32).cast::<c_void>(),
+			std::ptr::null_mut(),
+		)
+	};
+	if status != sane::SANE_Status_SANE_STATUS_GOOD {
+		bail!("Failed to get control option (option count) {}", status);
+	}
+	println!("Number of available options: {}.", option_count_value);
+
+	for option_num in 1..option_count_value {
+		let option_descriptor =
+			unsafe { libsane.sane_get_option_descriptor(device_handle.0, option_num) };
+		if option_descriptor.is_null() {
+			bail!("Failed to get option count");
+		}
+
+		unsafe {
+			info!(
+				"{:?}",
+				CStr::from_ptr((*option_descriptor).title).to_string_lossy()
+			);
+			info!(
+				"\t{:?}",
+				CStr::from_ptr((*option_descriptor).name).to_string_lossy()
+			);
+			info!(
+				"\t{:?}",
+				CStr::from_ptr((*option_descriptor).desc).to_string_lossy()
+			);
+			info!("\t{:?}", (*option_descriptor).type_);
+			info!("\t{:?}", (*option_descriptor).unit);
+			info!("\t{:?}", (*option_descriptor).size);
+			info!("\t{:?}", (*option_descriptor).cap);
+			info!("\t{:?}", (*option_descriptor).constraint_type);
+		}
+
+		unsafe {
+			if CStr::from_ptr((*option_descriptor).name)
+				== CStr::from_bytes_with_nul_unchecked(b"resolution\0")
+			{
+				info!("Setting resolution");
+
+				let mut option_value = match (*option_descriptor).type_ {
+					sane::SANE_Value_Type_SANE_TYPE_INT | sane::SANE_Value_Type_SANE_TYPE_FIXED => {
+						// 300 DPI
+						300_i32
+					}
+					type_ => {
+						bail!(
+							"Resolution type should only be either int or fixed. It's {}.",
+							type_
+						);
+					}
+				};
+
+				info!("Setting scan resolution to 300 dpi.");
+				let mut additional_status: sane::SANE_Int = 0;
+				let status: sane::SANE_Status = libsane.sane_control_option(
+					device_handle.0,
+					option_num,
+					sane::SANE_Action_SANE_ACTION_SET_VALUE,
+					(&mut option_value as *mut i32).cast::<c_void>(),
+					&mut additional_status as *mut sane::SANE_Int,
+				);
+				if status != sane::SANE_Status_SANE_STATUS_GOOD {
+					bail!("Failed to get control option (option count) {}", status);
+				}
+				info!("Additional status: {:b}.", additional_status);
+			}
+		}
+	}
+
+	let status = unsafe { libsane.sane_start(device_handle.0) };
+	if status != sane::SANE_Status_SANE_STATUS_GOOD {
+		bail!("Failed to initiate image acquisition {}.", status);
+	}
+
+	let mut sane_parameters = sane::SANE_Parameters {
+		format: 0,
+		last_frame: 0,
+		bytes_per_line: 0,
+		pixels_per_line: 0,
+		lines: 0,
+		depth: 0,
+	};
+
+	let status = unsafe {
+		libsane.sane_get_parameters(
+			device_handle.0,
+			&mut sane_parameters as *mut sane::SANE_Parameters,
+		)
+	};
+	if status != sane::SANE_Status_SANE_STATUS_GOOD {
+		bail!("Failed to get scan parameters {}.", status);
+	}
+
+	info!("Print parameters:");
+	info!("\tFormat {}.", sane_parameters.format);
+	info!("\tLast Frame {}.", sane_parameters.format);
+	info!("\tBytes per line {}.", sane_parameters.format);
+	info!("\tPixels per line {}.", sane_parameters.format);
+	info!("\tLines {}.", sane_parameters.format);
+	info!("\tDepth {}.", sane_parameters.format);
+
+	// 10 MB buf for the image
+	let mut image = Vec::<u8>::with_capacity(10 * 1024 * 1024);
+
+	let mut buf = Vec::<u8>::with_capacity(1024 * 1024 * 1024);
+	let mut bytes_written = 0_i32;
+	loop {
+		let mut sane_parameters = sane::SANE_Parameters {
+			format: 0,
+			last_frame: 0,
+			bytes_per_line: 0,
+			pixels_per_line: 0,
+			lines: 0,
+			depth: 0,
+		};
+		let status = unsafe {
+			libsane.sane_get_parameters(
+				device_handle.0,
+				&mut sane_parameters as *mut sane::SANE_Parameters,
+			)
+		};
+		if status != sane::SANE_Status_SANE_STATUS_GOOD {
+			bail!("Failed to get scan parameters {}.", status);
+		}
+
+		info!("Print parameters:");
+		info!("\tFormat {}.", sane_parameters.format);
+		info!("\tLast Frame {}.", sane_parameters.format);
+		info!("\tBytes per line {}.", sane_parameters.format);
+		info!("\tPixels per line {}.", sane_parameters.format);
+		info!("\tLines {}.", sane_parameters.format);
+		info!("\tDepth {}.", sane_parameters.format);
+
+		unsafe {
+			buf.set_len(0);
+		}
+		let status = unsafe {
+			libsane.sane_read(
+				device_handle.0,
+				buf.as_mut_ptr(),
+				buf.capacity() as i32,
+				&mut bytes_written as *mut i32,
+			)
+		};
+		match status {
+			sane::SANE_Status_SANE_STATUS_EOF => {
+				debug!("EOF ({} bytes read).", bytes_written);
+				break;
+			}
+			sane::SANE_Status_SANE_STATUS_GOOD => {
+				debug!("Good ({} bytes read).", bytes_written);
+			}
+			status => {
+				bail!("Failed to read data {}.", status);
+			}
+		}
+
+		unsafe {
+			buf.set_len(bytes_written as usize);
+		}
+		image.extend_from_slice(&buf);
+	}
+	unsafe {
+		libsane.sane_cancel(device_handle.0);
+	}
+
+	info!("Scan completed. Saving to file.");
+	std::fs::write("./scanned_document", image.as_slice())?;
+	std::fs::write("./scanned_document2", buf.as_slice())?;
+
 	Ok(())
+}
+
+struct Device<'a>(pub sane::SANE_Handle, &'a sane::libsane);
+
+impl<'a> Device<'a> {
+	pub unsafe fn from_raw_handle(handle: sane::SANE_Handle, libsane: &'a sane::libsane) -> Self {
+		Device(handle, libsane)
+	}
+}
+
+impl Drop for Device<'_> {
+	fn drop(&mut self) {
+		unsafe { self.1.sane_close(self.0) }
+	}
 }
