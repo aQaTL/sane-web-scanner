7351311c76940b1e359a514fe3ae0a04adeff9b4
aQaTL <mmsoltys@outlook.com>
2021-08-08 13:26:02 +0200

    Add fun page

---
+++ frontend/pages/fun.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>Hiiiiii!</p>
+		<p>Co tam?</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "fun"
+	}
+</script>
+
+<style scoped>
+	p {
+		font-size: 30pt;
+	}
+</style>
